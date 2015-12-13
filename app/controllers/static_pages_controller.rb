1fe74eb4c56d60cca6695f35937e7049c8df973c
Adam <the_doubleyou@yahoo.ca>
2015-12-12 19:33:43 -0700

    contact mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,15 @@ class StaticPagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.limit(3)
   end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'the_doubleyou@yahoo.ca',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver
+end
+
 end
