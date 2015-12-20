d17dfa7ea468344d44e31e2a7a5b8c73876ba95c
Adam <the_doubleyou@yahoo.ca>
2015-12-20 16:39:38 -0700

    star rating

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  resources :products do
+  resources :comments
+end
+
   resources :orders, only: [:index, :show, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
