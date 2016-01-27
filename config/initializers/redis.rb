003626fbdbc4692e80637db5e26291c5b2816bab
Adam <the_doubleyou@yahoo.ca>
2016-01-26 21:22:01 -0700

    redis fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,2 @@
-if Rails.env.production?
-  Rails.configuration.redis = {
+
     $redis = Redis.new(url: ENV["REDIS_URL"])
-  }
-else
-  Rails.configuration.redis = {
-    $redis = Redis.new(:host => 'localhost', :port => 6379)
-  }
-end
